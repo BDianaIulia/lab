3699f7cb47796859fe3d269468c33f2ceb3391a6
BDianaIulia <32904058+BDianaIulia@users.noreply.github.com>
2017-10-31 15:33:54 +0200

    Add files via upload

---
+++ Lab1_S1_P2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MTP(A,B) A*B
+int main()
+{   int x, y, z;
+    scanf("%d""%d", &x, &y);
+    z= MTP(x,y);
+    printf("%d\n", z);
+    printf("Hello world!\n");
+    return 0;
+}
